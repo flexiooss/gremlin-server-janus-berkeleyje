1c4292cf3beccea12febf2b6e9a1d3c854c1c431
nelt <nel@flexio.fr>
2019-03-13 16:46:54 +0100

    gremlin-server-janus-berkeleyje:0.3.1

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 VERSION="0.3.1"
 ARCHIVE="janusgraph-${VERSION}-hadoop2.zip"
 
-FILE_OK=$(sha256sum --check $ARCHIVE.sha256)
+sha256sum --check $ARCHIVE.sha256
+FILE_OK=$?
 if [ "$FILE_OK" -ne "0" ]; then
     rm -f
     wget https://github.com/JanusGraph/janusgraph/releases/download/v$VERSION/$ARCHIVE
